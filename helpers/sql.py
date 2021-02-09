df1b9a2d0185ee3396c25285c24af4e7e1331a2b
Marco Frisoli <marcof787@gmail.com>
2021-02-09 23:26:15 +0000

    Staging of data s3 to Redshift working

--- helpers/sql.py
+++ helpers/sql.py
@@ -1,3 +1,4 @@
+# Create Tables
 staging_events_create_table = """
     CREATE TABLE IF NOT EXISTS staging_events (
         index NUMERIC,
@@ -9,23 +10,19 @@ staging_events_create_table = """
         Latitude TEXT,
         Longitude TEXT,
         major_city BOOLEAN
-    )
+    );
 """
-
-# Fact Tables BIGINT IDENTITY(0,1)
 
 readings_by_city_create_table = """
     CREATE TABLE IF NOT EXISTS readings_by_city (
-        by_city_id SERIAL,
+        by_city_id BIGINT IDENTITY(0,1),
         time_id NUMERIC,
         city_id NUMERIC,
         avg_temp NUMERIC,
         avf_temp_uncertainty NUMERIC,
         major_city BOOLEAN
-    )
+    );
 """
-
-# Dimension tables
 
 time_create_table = """
     CREATE TABLE IF NOT EXISTS time (
@@ -35,27 +32,20 @@ time_create_table = """
         day SMALLINT,
         weekday SMALLINT,
         week SMALLINT
-    )
+    );
 """
 
 cities_create_table = """
     CREATE TABLE IF NOT EXISTS cities (
-        city_id SERIAL,
+        city_id BIGINT IDENTITY(0,1),
         country_id NUMERIC,
         city TEXT,
         latitude TEXT,
         longitude TEXT
-    )
+    );
 """
 
-sql_create_tables = [cities_create_table,
-                     time_create_table,
-                     readings_by_city_create_table,
-                     staging_events_create_table
-                    ]
-
 # Insert SQL statements
-
 staging_insert = """
     COPY staging_events (
         index,
@@ -69,6 +59,31 @@ staging_insert = """
         major_city
     )
     FROM  '{}'
+    CREDENTIALS 'aws_iam_role={}'
+    REGION '{}'
     DELIMITER ','
-    CSV HEADER
+    DATEFORMAT 'auto'
+    IGNOREHEADER 1
+    ;
 """
+# Drop Tables
+
+cities_table_drop = "DROP TABLE IF EXISTS cities;"
+
+time_table_drop = "DROP TABLE IF EXISTS time;"
+
+readings_by_city_table_drop = "DROP TABLE IF EXISTS readings_by_city;"
+
+staging_events_table_drop = "DROP TABLE IF EXISTS staging_events;"
+
+sql_create_tables = [cities_create_table,
+                     time_create_table,
+                     readings_by_city_create_table,
+                     staging_events_create_table
+                    ]
+
+sql_drop_tables = [cities_table_drop,
+                   time_table_drop, 
+                   readings_by_city_table_drop, 
+                   staging_events_table_drop
+                   ]
