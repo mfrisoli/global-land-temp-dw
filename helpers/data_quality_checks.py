8f0c601d68ecce43600abed266afe9ec15b84d10
Marco Frisoli <marcof787@gmail.com>
2021-02-17 21:53:41 +0000

    test: Adding additiona tests
    Added fuction to test data integrity and data types in the Data Warehouse

--- helpers/data_quality_checks.py
+++ helpers/data_quality_checks.py
@@ -1,39 +1,89 @@
 from psycopg2.errors import UndefinedTable
+import psycopg2
 
 # Perform quality checks here
 def data_quality_check(conn, table):
     """
-    Function that check if table has rows
+    Function that check if table is not empty and has rows
 
     Arguments:
     conn: <object> Posgres connection
-    table: <list> list of tables from the schema
+    table: str table name from the schema
 
     returns None
     """
     # Get cursor from connection
     cur = conn.cursor()
-
-    sql_test = """
-        SELECT COUNT(*) 
+    
+    sql_test_data = """
+        SELECT *
         FROM {}
         LIMIT 10;
         """.format(table)
-    try:
-        cur.execute(sql_test)
-        data_test = cur.fetchall()
 
-        if len(data_test) < 1:
+    sql_test_count = """
+        SELECT COUNT(*) 
+        FROM {};
+        """.format(table)
+    try:
+        cur.execute(sql_test_count)
+        data_test_count = cur.fetchall()
+
+        cur.execute(sql_test_data)
+        data_test_data = cur.fetchall()
+
+        if len(sql_test_data) < 1:
             raise ValueError("Table '{}' is empty".format(table))
 
-        if data_test[0][0] < 1:
-            raise ValueError("Table '{}' has no rows".format(table))
+        if data_test_count[0][0] < 1:
+            raise ValueError("Table '{}' has no rows".format(table))    
 
         print('Data Quality SUCCESS for table: {}. Total rows: {}'\
-                .format(table, data_test[0][0]))
-
+                .format(table, data_test_count[0][0]))
 
     except UndefinedTable as e:
+        # Close Connection
+        conn.close()
+        raise Exception(e)
+
+
+
+def data_integrity_check(conn, table, column_data_types):
+    """
+    Function that check if table has rows
+
+    Arguments:
+    conn: <object> Posgres connection
+    table: str table name from the schema
+    column_data_types: list of coumn data types
+
+    returns: None
+    """
+    # Get cursor from connection
+    cur = conn.cursor()
+
+    sql_test_integrity = """
+        SELECT * 
+        FROM {}
+        LIMIT 10;
+        """.format(table)
+
+    try:
+        cur.execute(sql_test_integrity)
+        data_test_integrity = cur.fetchall()
+
+        for col, col_type in zip(data_test_integrity[0],column_data_types):
+
+            col_format = "<class '{}'>".format(col_type)
+
+            if not (str(type(col)) == col_format):
+
+                raise ValueError("Table '{}' with invalid data type, \
+                    should be {} but instead got {}".format(table, col_format, type(col)))
+
+        print('Data Integrity SUCCESS for table: {}'.format(table))
+
+    except Exception as e:
         # Close Connection
         conn.close()
         raise Exception(e)
